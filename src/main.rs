c55706135fa4e50cbb56de396b68999f6c945e1c
Christopher Gundler <c.gundler@mail.de>
2017-01-29 13:09:39 +0100

    Introduce boilerplate for executable

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,52 @@ extern crate dtmf;
 extern crate hound;
 extern crate sample;
 
-use sample::Sample;
 use dtmf::Message;
 
-fn encode(message: Message) {
+fn encode(message: Message) -> bool {
     use dtmf::encoder::MessageEncoder;
+    use hound::{WavWriter, WavSpec};
 
-    let spec = hound::WavSpec {
+    let spec = WavSpec {
         channels: 1,
         sample_rate: 44100,
         bits_per_sample: 32,
         sample_format: hound::SampleFormat::Int,
     };
 
-    let encoder = MessageEncoder::new(&message, 44_100.0).map(|s| sample::conv::f64::to_i32(s[0]));
-    //use ::sample::signal::{rate, Sine, AddAmp, ConstHz, Signal};
-    //let encoder = rate(44_100.0).const_hz(852.0).sine().scale_amp(0.5).take(44_100).map(|s| sample::conv::f64::to_i32(s[0]));
+    if let Ok(mut writer) = WavWriter::create("dtmf.wav", spec) {
+        for sample in MessageEncoder::new(&message, 44_100.0)
+            .map(|s| sample::conv::f64::to_i32(s[0])) {
 
-    let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
-    for sample in encoder {
-        writer.write_sample(sample);
+            if writer.write_sample(sample).is_err() {
+                return false;
+            }
+        }
+        true
+    } else {
+        false
     }
 }
 
 fn main() {
-
+    // Get arguments
     let args: Vec<_> = ::std::env::args().skip(1).collect();
+
     match args.len() {
         0 => unimplemented!(),
         1 => {
-            match args.first().unwrap().parse() {
+            let success = match args.first().unwrap().parse() {
                 Ok(message) => encode(message),
-                Err(_) => println!("[ERROR] Invalid message"),
+                Err(_) => {
+                    println!("[ERROR] Invalid message");
+                    return;
+                }
             };
+
+            if !success {
+                println!("[ERROR] Error during writing the file.")
+            }
         }
-        _ => println!("[ERROR] Please insert an argument."),
+        _ => println!("[ERROR] Please specify an argument."),
     }
 }
