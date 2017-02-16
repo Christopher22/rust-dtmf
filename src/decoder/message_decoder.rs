c247836a0252a95209379e583458cd3ecd8acf8c
Christopher Gundler <c.gundler@mail.de>
2017-02-16 12:34:24 +0100

    "Rustify" MessageDecoder

--- src/decoder/message_decoder.rs
+++ src/decoder/message_decoder.rs
@@ -1,5 +1,4 @@
-use ::Message;
-use ::encoder::MessageEncoder;
+use Message;
 
 use super::decode_signal;
 
@@ -17,39 +16,40 @@ use super::decode_signal;
 /// let mut target_message = Message::default();
 ///
 /// let data = MessageEncoder::new(&message, 48000.);
-/// decode_message(&mut target_message, &data, 48000.);
+/// decode_message(data.map(|x| x[0]), &mut target_message, 48000.);
 ///
 /// assert_eq!(message, target_message);
 /// ```
-pub fn decode_message(message: &mut Message, encoded_message: &MessageEncoder, sample_rate: f64) {
+pub fn decode_message<S, I>(sample_iter: S, message: &mut Message, sample_rate: f64)
+    where S: IntoIterator<Item = f64, IntoIter = I>,
+          I: Iterator<Item = f64> + ExactSizeIterator
+{
+    let mut samples = sample_iter.into_iter();
+    let mut length = samples.len();
 
-    // let mut signals = VecDeque::new();
-    let length = encoded_message.clone().count();
     let signal_duration = (message.signal_duration() * sample_rate) as usize;
     let silence_duration = (message.silence_duration() * sample_rate) as usize;
 
-    // split audio-message into audio-signals on pauses
-    // first signal_duration
-    if length >= signal_duration {
-        let signal = decode_signal((&(encoded_message.clone()
-                                       .take(signal_duration)
-                                       .map(|x| x[0])
-                                       .collect::<Vec<f64>>())),
+    let mut first_signal = true;
+    while length > 0 {
+
+        // Decode each signal
+        let signal = decode_signal(samples.by_ref()
+                                       .skip(match first_signal {
+                                           true => {
+                                               first_signal = false;
+                                               0
+                                           }
+                                           false => {
+                                               length -= silence_duration;
+                                               silence_duration
+                                           }
+                                       })
+                                       .take(signal_duration),
                                    sample_rate);
 
+        // Add the signal
         message.enqueue(signal);
-    }
-
-    // other signals
-    let mut index = signal_duration;
-    while (index + signal_duration + silence_duration) <= length {
-        let signal = decode_signal((&(encoded_message.clone()
-                                       .take(signal_duration)
-                                       .map(|x| x[0])
-                                       .collect::<Vec<f64>>())),
-                                   sample_rate);
-
-        message.enqueue(signal);
-        index += signal_duration + silence_duration;
+        length -= signal_duration;
     }
 }
